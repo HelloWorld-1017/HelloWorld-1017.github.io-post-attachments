eadc43e86c77602fe41dfdfdf019baf22efd3002
花魑 <50327243+QinghuaMa@users.noreply.github.com>
2024-03-18 06:46:13 +0800

    Update script3.m

--- archive/2023/2023-10-17 Built-in Color Names in LaTeX xcolar Package/script3.m
+++ archive/2023/2023-10-17 Built-in Color Names in LaTeX xcolar Package/script3.m
@@ -4,21 +4,19 @@ colorTable = readtable("xcolors.csv");
 colorStruct = table2struct(colorTable);
 colors = struct();
 
+numPerFig = 36;
 vertSpace = 20;
-colorBoxPositions = repmat([0,0,70,15],36,1)-[zeros(36,1),vertSpace*(1:36)',zeros(36,1),zeros(36,1)];
+colorBoxPositions = repmat([0,0,70,15],numPerFig,1)-[zeros(numPerFig,1),vertSpace*(1:numPerFig)',zeros(numPerFig,1),zeros(numPerFig,1)];
 textxPosition_color1 = 100;
 textxPosition_color2 = textxPosition_color1+170;
-textxPosition_color3 = textxPosition_color2+160;
-textxPosition_color4 = textxPosition_color3+160;
-textyPositions = 4-vertSpace*(1:36)';
-
+textxPosition_color3 = textxPosition_color2+170;
+textxPosition_color4 = textxPosition_color3+170;
 
 FontSize = 12;
 FontName = "Helvetiva";
 pic_idx = 0;
 for idx = 1:height(colorStruct)
-    i = mod(idx-1,36)+1;
-    disp(i)
+    i = mod(idx-1,numPerFig)+1;
     if i == 1
         figure("Color","w","Position",[682,42,902,953])
         hold(gca,"on")
@@ -26,7 +24,6 @@ for idx = 1:height(colorStruct)
         pic_idx = pic_idx+1;
     end
 
-    
     colorName = colorStruct(idx).color;
     RGBtuple1 = [colorStruct(idx).r,colorStruct(idx).g,colorStruct(idx).b];
     RGBtuple2 = round(RGBtuple1*255);
@@ -36,24 +33,27 @@ for idx = 1:height(colorStruct)
     colors(idx).RGBtuple2 = {RGBtuple2};
     colors(idx).RGBHex = {RGBHEX};
 
-    rectangle("Position",colorBoxPositions(i,:), ...
-        "FaceColor",RGBtuple1,'Curvature',0.5,"LineWidth",1.2)
-    text(textxPosition_color1,textyPositions(i), ...
+    r = rectangle("Position",colorBoxPositions(i,:), ...
+        "FaceColor",RGBtuple1,"Curvature",0.5,"LineWidth",1.2);
+
+    textyPosition = r.Position(2)+r.Position(4)/2;
+    text(textxPosition_color1,textyPosition, ...
         colorName,"FontSize",FontSize,"FontName",FontName)
-    text(textxPosition_color2,textyPositions(i), ...
-        sprintf("(%.2f %.2f %.2f)",RGBtuple1), ...
+    text(textxPosition_color2,textyPosition, ...
+        sprintf("(%.3f %.3f %.3f)",RGBtuple1), ...
         "FontSize",FontSize,"FontName",FontName)
-    text(textxPosition_color3,textyPositions(i), ...
+    text(textxPosition_color3,textyPosition, ...
         sprintf("(%-3d %-3d %-3d)",RGBtuple2), ...
         "FontSize",FontSize,"FontName",FontName)
-    text(textxPosition_color4,textyPositions(i), ...
+    text(textxPosition_color4,textyPosition, ...
         sprintf("%s",RGBHEX), ...
         "FontSize",FontSize,"FontName",FontName)
     xlim([-10,600])
     axis(gca,"off")
 
-    %     if i == 36
-    %         exportgraphics(gca,sprintf("pic_%d.jpg",pic_idx),"Resolution",600)
-    %     end
+    if i == numPerFig
+        exportgraphics(gca,sprintf("pic_%d.jpg",pic_idx),"Resolution",600)
+    end
 end
 
+disp(struct2table(colors))
